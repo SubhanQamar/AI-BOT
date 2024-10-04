e078aa7259c4433cbf6bc36567f03d6465165435
SubhanQamar <subhan.qamar@gosign.de>
2024-10-04 15:19:27 +0500

    Initial Commit

---
+++ AI-BOT/AI_BOTApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_BOTApp.swift
+//  AI-BOT
+//
+//  Created by Subhan Qamar on 04/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct AI_BOTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI-BOTTests/AI_BOTTests.swift
@@ -0,0 +1,17 @@
+//
+//  AI_BOTTests.swift
+//  AI-BOTTests
+//
+//  Created by Subhan Qamar on 04/10/2024.
+//
+
+import Testing
+@testable import AI_BOT
+
+struct AI_BOTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
